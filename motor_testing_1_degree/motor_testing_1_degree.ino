d62efdc4b545da503a02e3ccbc4b2456627e2e51
robolux <HyperDrive@Hunters-MacBook-Air.local>
2017-10-28 22:47:34 -0500

    pin changes for stepper
    needed to change pins for stepper because 32-bit ADC were already using
    them

--- motor_testing_1_degree/motor_testing_1_degree.ino
+++ motor_testing_1_degree/motor_testing_1_degree.ino
@@ -5,16 +5,16 @@ int state;
 
 
 void setup() {                
+  pinMode(2, OUTPUT);
   pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
+  pinMode(A0, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  digitalWrite(2, LOW);
   digitalWrite(8, LOW);
-  digitalWrite(9, LOW);
-  digitalWrite(5,LOW);
-  digitalWrite(6,LOW);
-  digitalWrite(7,LOW);
+  digitalWrite(A0,0);
+  digitalWrite(A1,0);
+  digitalWrite(A2,0);
   Serial.begin(9600); //Open Serial connection for debugging
 
 }
@@ -32,9 +32,9 @@ void loop() {
 
 void test123()
 {
-    digitalWrite(9, HIGH);
+    digitalWrite(8, HIGH);
     delay(1);          
-    digitalWrite(9, LOW); 
+    digitalWrite(8, LOW); 
     delay(1);    
 }
 
